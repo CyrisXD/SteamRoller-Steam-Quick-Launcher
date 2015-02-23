9c7b34071d7d6a45ac970a7701cfbac478265d02
Oden (thesbros) <ops991923@gmail.com>
2015-02-22 17:31:56 -0800

    Fix indent

--- gulp/tasks/nw-build.js
+++ gulp/tasks/nw-build.js
@@ -4,8 +4,8 @@ var paths = require("../paths");
 var Builder = require("node-webkit-builder");
 
 var nw = new Builder({
-    files: paths.output + "**/*",
-    platforms: ["win", "osx", "linux"],
+	files: paths.output + "**/*",
+	platforms: ["win", "osx", "linux"],
 	buildDir: paths.builds,
 	macIcns: paths.output + "images/icon.icns",
 	winIco: paths.output + "images/icon.ico"
